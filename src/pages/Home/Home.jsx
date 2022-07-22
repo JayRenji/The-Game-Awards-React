95321d7d730909ad4386845c3f8ff82c0fc11e99
Jay <100322157+JayRenji@users.noreply.github.com>
2022-07-22 20:32:03 +0200

    assets and first changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,9 +2,29 @@ import React from 'react';
 import './Home.scss';
 
 function Home() {
-  return (
-    <div>Home works!</div>
-  )
+	return (
+		<div className='home-page'>
+			<p class='welcome'>WELCOME TO THE 2022 EDITION OF</p>
+			<p class='award'>THE GAME AWARDS!</p>
+
+			<div class='search'>
+				<input class='input' type='text' placeholder='Search Game or Genre' />
+			</div>
+			<div class='cards'>
+				<img src='{{ game.img }}' alt='{{ game.title }}' />
+				<div class='card__content'>
+					<h4 class='genre'> game.genre </h4>
+					<ul class='platform'>
+						<li> platform </li>
+					</ul>
+				</div>
+				<div class='card__buttons'>
+					<button>VOTE!</button>
+					<button>EDIT</button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
-export default Home
+export default Home;
