161a5c25d4e1a0bf3aeeb7bf8a4527d2b81ec8b7
Jay <100322157+JayRenji@users.noreply.github.com>
2022-07-22 21:00:38 +0200

    Update Home.jsx

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,7 +7,30 @@ import { getAllGames } from '../../redux/games/games.actions';
 
 function Home() {
 	return (
-		const dispatch = useDispatch();
+		<section>
+			<p class='welcome'>WELCOME TO THE 2022 EDITION OF</p>
+			<p class='award'>THE GAME AWARDS!</p>
+
+			<div class='search'>
+				<input class='input' type='text' placeholder='Search Game or Genre' />
+			</div>
+			<div class='cards'>
+				<img src='{{ game.img }}' alt='{{ game.title }}' />
+				<div class='card__content'>
+					<h4 class='genre'> game.genre </h4>
+					<ul class='platform'>
+						<li> platform </li>
+					</ul>
+				</div>
+				<div class='card__buttons'>
+					<button>VOTE!</button>
+					<button>EDIT</button>
+				</div>
+			</div>
+		</section>
+	);
+
+	const dispatch = useDispatch();
 	const params = useParams();
 	const navigate = useNavigate();
 	const { games, info, error, isLoading } = useSelector((state) => state.games);
@@ -27,9 +50,6 @@ function Home() {
 			))}
 		</>
 	);
-	);
-
-	
 }
 
 export default Home;
