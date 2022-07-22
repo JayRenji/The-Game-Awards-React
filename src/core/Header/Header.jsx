3f75043aef7595a5b425768a85bc3b261d1c888d
alvaroFGG <alvarofuentenebrogomez@gmail.com>
2022-07-22 14:49:15 +0200

    Header and Footer styled with router

--- src/core/Header/Header.jsx
+++ src/core/Header/Header.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { Route, Link, Routes, useLocation } from "react-router-dom";
+import './Header.scss';
 
 
 const Header = () => {
   return (
-    <div>
-        <Link to='/'>Home</Link>
-        <Link to='/podium'>Podium</Link>
-        <Link to='/management'>Management</Link>
-        <Link to='/login'>Login</Link>
+    <header>
+        
+
+        <div className="headercontent">
+        <Link to='/'><h1 className="headercontent__title neonText" >TGA2022</h1></Link>
+
+        <ul className="headercontent__menu">
+            <li><Link to='/' className='neonText'>Home</Link></li>
+            <li> <Link to='/podium' className='neonText'>Podium</Link></li>
+            <li> <Link to='/management' className='neonText'>Management</Link></li>
+            <li> <Link to='/login' className='neonText'>Login</Link> </li>
+        </ul>
+
     </div>
+    </header>
   )
 }
 
