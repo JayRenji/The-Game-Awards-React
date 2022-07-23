a027d480b2658d3d0335e92cdce2889c0eda8175
alvaroFGG <alvarofuentenebrogomez@gmail.com>
2022-07-23 13:49:46 +0200

    podium finished
    background img missing, waiting for other dev to upload his back img

--- src/pages/Podium/Podium.jsx
+++ src/pages/Podium/Podium.jsx
@@ -3,6 +3,9 @@ import './Podium.scss';
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllGames } from '../../redux/games/games.actions';
+import crown from '../../assets/img/crown.png';
+
+
 
 function Podium() {
   const {games} = useSelector(state => state.games);
@@ -16,19 +19,6 @@ function Podium() {
     getGames();
   }, []);
 
-
-  const print = () => {
-    const sortedGames = games.sort(compare).splice(0,3);
-    // return sortedGames;
-    return (
-      <>
-        {sortedGames.map((game) => 
-          <p>{game.title}</p>
-        )}
-      </>
-    )
-  }
-
   const compare = (a, b) => {
     if(a.votes < b.votes){
       return 1;
@@ -39,9 +29,26 @@ function Podium() {
     return 0;
   }
 
+  //sort all games and get only the first 3
+  const sortedGames = games.sort(compare).splice(0,3);
+  //change positions between first and second game to make the best game to render in the middle
+  [sortedGames[0], sortedGames[1]] = [sortedGames[1], sortedGames[0]];
 
   return (
-    <>{print()}</>
+    <section className='podium'>
+      <h2>Top 3 GOTY:</h2>
+      <img className='crown' src={crown}/>
+      <div className='podium__gotys'>
+        {sortedGames.map((game) =>
+          <div className='podium__gotys--game'>
+              <img src= { game.img }/>
+              <div className='div__votes'>
+                  <p className='votes'>{ game.votes } votes</p>
+              </div>
+          </div>
+        )}
+      </div>
+    </section>
   )
 }
 
